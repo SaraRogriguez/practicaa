50209bafa8cff48fefc6b2b570f81b80e1148594
SaraRodriguez <sarayuley00@gmail.com>
2024-11-26 10:41:43 -0500

    hola

---
+++ src/main/java/Clase/Claseuno.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Clase;
+
+/**
+ *
+ * @author USER
+ */
+public class Claseuno {
+    private String nombre;
+    private String apellido;
+    private String curso;
+
+    public Claseuno(String nombre, String apellido, String curso) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.curso = curso;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+    
+    
+    
+}
---
+++ src/main/java/sara/rodriguez/practica5/PRACTICA5.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package sara.rodriguez.practica5;
+
+/**
+ *
+ * @author USER
+ */
+public class PRACTICA5 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
